c12e8113ae3156a0f935e3e9e7c26d9f01441892
Lavish987 <81297619+Lavish987@users.noreply.github.com>
2022-08-18 10:07:54 +0530

    Delete responsive-watches-website-main directory

--- responsive-watches-website-main/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const scrollHeader = () =>{
-    const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
-
